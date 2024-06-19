a0370308b8982fe0fecd1e71a7a7ede4b80e75c4
lakshmiganthan123 <230401078@rajalakshmi.edu.in>
2024-06-19 10:12:34 +0530

    Create 5.2.py

---
+++ week-5/5.2.py
@@ -0,0 +1,2 @@
+Write a program to check if two strings are balanced. For example, strings s1 and s2 are balanced if all the characters in the s1 are present in s2. The character’s position doesn’t matter. If balanced display as "true" ,otherwise "false".
+print(input()in input())
