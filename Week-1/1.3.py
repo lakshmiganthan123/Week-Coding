83f76f71a9e49d9af8c3580e3d986604acaaeb24
lakshmiganthan123 <230401078@rajalakshmi.edu.in>
2024-06-19 08:41:21 +0530

    Create 1.3.py

---
+++ Week-1/1.3.py
@@ -0,0 +1,13 @@
+Write a simple python program to find the square root of a given floating point number. The output should be displayed with 3 decimal places.
+
+Sample Input:
+
+8.00
+
+Sample Output:
+
+2.828
+
+a=float(input())
+b=a**0.5
+print(f"{(a**0.5):.3f}")
