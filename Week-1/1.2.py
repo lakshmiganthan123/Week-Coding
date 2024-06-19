28a6367c356cc09eba499253031e35d4964b9ef7
lakshmiganthan123 <230401078@rajalakshmi.edu.in>
2024-06-19 08:37:05 +0530

    Create 1.2.py

---
+++ Week-1/1.2.py
@@ -0,0 +1,13 @@
+Ramesh’s basic salary is input through the keyboard. His dearness allowance is 40% of his basic salary, and his house rent allowance is 20% of his basic salary. Write a program to calculate his gross salary.
+
+Sample Input:
+
+10000
+
+Sample Output:
+
+16000
+
+a=int(input())
+b=(a*(40/100))+(a*(20/100))+a
+print(b)
