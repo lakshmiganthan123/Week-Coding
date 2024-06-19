19e9bc8a758336505a138d51750cc6a383f4a2fa
lakshmiganthan123 <230401078@rajalakshmi.edu.in>
2024-06-19 08:52:35 +0530

    Create 1.6.py

---
+++ Week-1/1.6.py
@@ -0,0 +1,18 @@
+Justin is a carpenter who works on an hourly basis. He works in a company where he is paid Rs 50 for an hour on weekdays and Rs 80 for an hour on weekends. He works 10 hrs more on weekdays than weekends. If the salary paid for him is given, write a program to find the number of hours he has worked on weekdays and weekends.
+
+Hint:
+
+If the final result(hrs) are in -ve convert that to +ve using abs() function
+
+The abs() function returns the absolute value of the given number.
+
+
+number = -20
+absolute_number = abs(number)
+print(absolute_number)
+# Output: 20
+a=int(input())
+we=abs((a-500)/130)
+wd=we+10
+print("weekdays %.2f"%wd)
+print("weekend %.2f"%we)
